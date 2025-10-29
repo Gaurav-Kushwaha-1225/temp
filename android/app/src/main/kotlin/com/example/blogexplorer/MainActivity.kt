c839b4775affdd532a45b73603498cb7c498ae47
Gaurav Kushwaha <gaurav.family04@gmail.com>
2025-10-30 01:57:42 +0530

    clear

--- android/app/src/main/kotlin/com/example/blogexplorer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.blogexplorer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/enums.dart
+++
@@ -1,6 +0,0 @@
-enum NoteSortBy {
-  dateDesc,
-  dateAsc,
-  titleAsc,
-  titleDesc,
-}
